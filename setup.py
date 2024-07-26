a660abd5a31c07325ed058a5c0be9ffb226d458c
Gaurav Gaurav <zen@Zen-master.local>
2024-07-26 12:36:55 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stats",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[pandas,numpy],
+    author="Gaurav G",
+    author_email="lotusheart2016@gmail.com",
+    description="A package for evaluating model performance metrics",
+    url="https://github.com/yourusername/your_project",
+)
