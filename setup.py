fd313b139beb57c8f86bc153e4d19338006514e1
Gaurav Gaurav <zen@Zen-master.local>
2024-07-26 14:29:23 +0100

    Updated package version and corrected __init__ file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ggstats",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=['pandas>=1.0.0','numpy>=1.18.0'],
     author="Gaurav G",
