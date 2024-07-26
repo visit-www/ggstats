a2f1d3733ce6a9b229063b8dfafca132cadf71ce
Gaurav Gaurav <zen@Zen-master.local>
2024-07-26 13:30:23 +0100

    Add readme and licence file. Also update swetup.py file fix the eerror in the install_requies in setup.pv and added classified and python lines in setup.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# Stats
+
+A package for evaluating metrics.
+This packages can calculate specificty and sensitivty if TP, FN and FP are provided.
+The package can also calcualte accuracy and PPV from both basic values such as TP,FN,FP,and TN but also from sensitivty and specificty and prevlance.
+
+## Installation
+
+```sh
+pip install stats
--- setup.py
+++ setup.py
@@ -4,9 +4,15 @@ setup(
     name="stats",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[pandas,numpy],
+    install_requires=['pandas>=1.0.0','numpy>=1.18.0'],
     author="Gaurav G",
     author_email="lotusheart2016@gmail.com",
     description="A package for evaluating model performance metrics",
     url="https://github.com/visit-www/stats.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
